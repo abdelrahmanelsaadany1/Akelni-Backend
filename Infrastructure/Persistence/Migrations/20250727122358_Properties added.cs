5b92210c846df23d6843c7673ba8dde84588686e
Abdelrahman Hossam Eldin Zaki Ahmed <abdelrahmanhossam2222@gmail.com>
2025-07-27 15:26:01 +0300

    new migration

---
+++ Infrastructure/Persistence/Migrations/20250727122358_Properties added.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Propertiesadded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Restaurants",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsOpen",
+                table: "Restaurants",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OpeningHours",
+                table: "Restaurants",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Restaurants");
+
+            migrationBuilder.DropColumn(
+                name: "IsOpen",
+                table: "Restaurants");
+
+            migrationBuilder.DropColumn(
+                name: "OpeningHours",
+                table: "Restaurants");
+        }
+    }
+}
