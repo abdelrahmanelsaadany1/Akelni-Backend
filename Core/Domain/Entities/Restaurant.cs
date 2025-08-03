0c5ee259cf7ed022d84b6cb07c3e27667675f491
Zeyad Mohamed <zieadhammad15@gmail.com>
2025-08-03 12:24:16 +0300

    apply Sieve inside restaurant entity

--- Core/Domain/Entities/Restaurant.cs
+++ Core/Domain/Entities/Restaurant.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.Entities.Identity;
+using Sieve.Attributes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,13 @@ namespace Domain.Entities
 {
     public class Restaurant:BaseEntity
     {
-      
+
+        [Sieve(CanSort = true)]
         public string Name { get; set; }
         public string Description { get; set; }
         public string Location { get; set; }
+
+        [Sieve(CanSort = true)]
         public double Rating { get; set; }
         public string ImageUrl { get; set; }
         public string ChefId { get; set; }
